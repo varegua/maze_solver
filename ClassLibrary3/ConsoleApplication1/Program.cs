fa9a1687d8bdfc0bba7c1ff776fb36b044550213
Benjamin <vannierbenjamin@gmail.com>
2017-02-17 14:03:51 +0100

    Revert "recup"
    This reverts commit db40df4760e77180e1e5af3747f4a32b197e49e1.

---
+++ ClassLibrary3/ConsoleApplication1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary3.mazeSolverService;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Test
+    {
+        static void Main()
+        {   
+            GameClient client = new GameClient("BasicHttpBinding_IGame");
+            Console.WriteLine(client);
+            PlayerGame player = client.CreateGame(Difficulty.Easy, "Test");
+            Console.WriteLine(player.Player.Name);
+            Console.WriteLine(player.Player.NbMove);
+            Console.WriteLine(player.Player.CurrentPosition.X);
+            Console.WriteLine(player.Player.CurrentPosition.Y);
+            Console.WriteLine(player.Player.SecretMessage);
+            Console.WriteLine(player.Player.VisibleCells);
+            Cell[] visibleCells = player.Player.VisibleCells;
+            foreach (Cell C in visibleCells) {
+                Console.WriteLine("cellule :");
+                Console.WriteLine(C.CellType);
+                Console.WriteLine(C.Position.X);
+                Console.WriteLine(C.Position.Y);
+            }
+            Thread.Sleep(500);
+            Player P1 = player.Player;
+            Console.WriteLine(player.Key);
+            P1 = client.MovePlayer(player.Key, player.Player.Key, Direction.Down);
+            Console.WriteLine(P1.Name);
+            Console.WriteLine(P1.NbMove);
+            Console.WriteLine(P1.CurrentPosition.X);
+            Console.WriteLine(P1.CurrentPosition.Y);
+            Console.WriteLine(P1.SecretMessage);
+            Console.WriteLine(P1.VisibleCells);
+            visibleCells = P1.VisibleCells;
+            foreach (Cell C in visibleCells)
+            {
+                Console.WriteLine("cellule :");
+                Console.WriteLine(C.CellType);
+                Console.WriteLine(C.Position.X);
+                Console.WriteLine(C.Position.Y);
+            }
+            while (true) { }
+
+            // Use the 'client' variable to call operations on the service.
+
+            // Always close the client.
+
+   
+        }
+    }
+}
---
+++ ClassLibrary3/WpfApplication1/StartGamePage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary3.mazeSolverService;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Logique d'interaction pour StartGamePage.xaml
+    /// </summary>
+    public partial class StartGamePage : Page
+    {
+        public StartGamePage()
+        {
+            InitializeComponent();
+
+            foreach(var difficulty in Enum.GetValues(typeof(Difficulty)))
+            {
+                cBDifficulty.Items.Add(difficulty);
+            }
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            Difficulty difficulty =(Difficulty) cBDifficulty.SelectedItem;
+            String name = tBPseudo.Text;
+            GamePage page = new GamePage(name, difficulty);
+            this.NavigationService.Navigate(page);
+        }
+    }
+}
